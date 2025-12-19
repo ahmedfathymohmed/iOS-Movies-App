ff099b1be381813381746d6279c1dfa4e0c2b74d
Ayman F.Mohammed <aymanfathy367@gmail.com>
2025-12-19 17:00:35 +0200

    Initial Commit

---
+++ Movies App Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies App Task
+//
+//  Created by Ayman Fathy on 19/12/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Movies App TaskTests/Movies_App_TaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  Movies_App_TaskTests.swift
+//  Movies App TaskTests
+//
+//  Created by Ayman Fathy on 19/12/2025.
+//
+
+import Testing
+@testable import Movies_App_Task
+
+struct Movies_App_TaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
