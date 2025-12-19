29183d9aa772981596d2cc6a5f5e00c878b7400b
Ayman F.Mohammed <aymanfathy367@gmail.com>
2025-12-19 17:13:56 +0200

    Project setup

--- Movies App Task/Scence/Home/HomeViewController.swift
+++ Movies App Task/Scence/Home/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Movies App Task
 //
 //  Created by Ayman Fathy on 19/12/2025.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-
 }
-
